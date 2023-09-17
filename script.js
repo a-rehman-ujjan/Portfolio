089631deadbe1b797ba6bffb428d613beb8c9b66
a_rehman_ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2023-09-18 01:32:40 +0500

    Update script.js
    Modified - Added Preview Functionality to Portfolio Images

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const App_Design = [`<img src="files/data/AD/01.png">`,`<img src="files/data/AD/
 
 const Development = [`<img src="files/data/DV/01.png">`]
 
-const Graphic_Design = [`<img src="files/data/GD/01.png">`,`<img src="files/data/GD/02.png">`]
+const Graphic_Design = [`<img src="files/data/GD/01.png">`,`<img src="files/data/GD/02.png">`,`<img src="files/data/GD/03.png">`]
 
 const Wordpress = []
 
@@ -109,3 +109,17 @@ const cc = (array, pg) => {
   }
 }
 cc(All, 1);
+
+
+const preview = (id)=>{
+  let pimg_data = document.getElementById(id).innerHTML
+  let pimg_src;
+  if(pimg_data != '<span class="material-symbols-rounded locked">network_intelligence_history</span>'){
+  pimg_src = document.getElementById(id).children[0].attributes.src.value
+  document.getElementById("preview-div").style.display = "flex"
+  document.getElementById("preview-div").innerHTML = '<div id="close-c" onclick="close_preview()"></div>' + `<img src="${pimg_src}">` + '<div id="pd-close-button"><span class="material-symbols-rounded" onclick="close_preview()">close</span></div>'
+  }
+}
+const close_preview = ()=>{
+  document.getElementById("preview-div").style.display = "none"
+}
